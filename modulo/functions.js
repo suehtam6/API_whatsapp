60ca2eab60c61e519c420eeca73cc5f28d369644
MATHEUS LUCAS DE FREITAS ZACARIAS <25203742@CFP127ALUNO.LOCAL>
2026-04-09 15:30:07 -0300

    Fazendo as functions de getListaMensagem e getContatoMensagem

--- modulo/functions.js
+++ modulo/functions.js
@@ -15,48 +15,48 @@ const getDadosUsuario = function () {
     return listaDadosUsuario
 }
 
-const getContaUsuario = function(numero){
+const getContaUsuario = function (numero) {
     let listaConta = []
     let entradaNumero = numero
-    contato.forEach(function(itemConta){
-        if(entradaNumero == itemConta.number){
+    contato.forEach(function (itemConta) {
+        if (entradaNumero == itemConta.number) {
             dados = {
-                nome : itemConta.account,
-                nick : itemConta.nickname,
-                numero : itemConta.number,
-                foto : itemConta["profile-image"],
-                corFundo : itemConta.background,
-                StartConta : itemConta["created-since"].start,
-                EndConta : itemConta["created-since"].end
+                nome: itemConta.account,
+                nick: itemConta.nickname,
+                numero: itemConta.number,
+                foto: itemConta["profile-image"],
+                corFundo: itemConta.background,
+                StartConta: itemConta["created-since"].start,
+                EndConta: itemConta["created-since"].end
             }
 
             listaConta.push(dados)
-        }else{
+        } else {
             return false
         }
 
-        
+
 
     })
-    if(listaConta == ''){
+    if (listaConta == '') {
         return false
-    }else{
+    } else {
         return listaConta
     }
-    
+
 }
 
-const getContatosUsuario = function(numero){
+const getContatosUsuario = function (numero) {
     let listaContato = []
     let entradaNumero = numero
 
-    contato.forEach(function(itemContato){
-        if(entradaNumero == itemContato.number){
-            itemContato.contacts.forEach(function(itemDescricaoContato){
+    contato.forEach(function (itemContato) {
+        if (entradaNumero == itemContato.number) {
+            itemContato.contacts.forEach(function (itemDescricaoContato) {
                 dados = {
-                    nome : itemDescricaoContato.name,
-                    descricao : itemDescricaoContato.description,
-                    foto : itemDescricaoContato.image
+                    nome: itemDescricaoContato.name,
+                    descricao: itemDescricaoContato.description,
+                    foto: itemDescricaoContato.image
                 }
 
                 listaContato.push(dados)
@@ -68,12 +68,68 @@ const getContatosUsuario = function(numero){
     return listaContato
 }
 
-const getListaMensagem = function(numero){
+const getListaMensagem = function (numero) {
     let listaMensagem = []
     let entradaNumero = numero
+
+    contato.forEach(function (itemUsuario) {
+        if (entradaNumero == itemUsuario.number) {
+            itemUsuario.contacts.forEach(function (itemMensagem) {
+                dados = {
+                    mensagem: itemMensagem.messages
+                }
+
+                listaMensagem.push(dados)
+            })
+        }
+
+    })
+    if (listaMensagem == '' || listaMensagem == null) {
+        return false
+    } else {
+        return listaMensagem
+    }
+}
+
+const getContatosMensagem = function (numero) {
+    let listaDadosContatos = []
+    let entradaNumero = numero
+
+    contato.forEach(function (itemContato) {
+
+        if (entradaNumero == itemContato.number) {
+            dados = {
+                nome: itemContato.account,
+                numero: itemContato.number,
+
+            }
+
+            listaDadosContatos.push(dados)
+            
+            mensagens = itemContato.contacts.forEach(function (itemMensagem) {
+                dadosMensagem = {
+                    mensagem: itemMensagem.messages
+                }
+
+                listaDadosContatos.push(dadosMensagem)
+
+            })
+
+            
+            
+
+        }
+
+
+    })
+
+    return listaDadosContatos
+
 }
 
 
-// console.log(getDadosUsuario())
-// console.log(getContaUsuario(11966578996))
-// console.log(getContatosUsuario(11966578996))
+// console.log(getDadosUsuario()) 1
+// console.log(getContaUsuario(11966578996)) 2
+// console.log(getContatosUsuario(11966578996)) 3
+//console.log(getListaMensagem(11966578996)) 4
+console.log(getContatosMensagem(11966578996))
